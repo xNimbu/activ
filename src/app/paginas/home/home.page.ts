adb5ebef8365b3f0bb6dc4fbc8883b6534a24353
xNimbu <lui.illanes@duocuc.cl>
2024-09-12 14:32:15 -0300

    home

--- src/app/paginas/home/home.page.ts
+++ src/app/paginas/home/home.page.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  usuario: string = '';
+  precioEntrada: number = 15000;
+  cantidadEntradas: number = 1;
+  totalPagar: number = 0;
+  compraExitosa: boolean = false;
+  mensajeExito: string = '';
 
   constructor() {}
+
+  ngOnInit(){
+    this.usuario = localStorage.getItem('usuario') || 'Invitado';
+    this.calcularTotal();
+  }
+
+  calcularTotal() {
+    this.totalPagar = this.precioEntrada * this.cantidadEntradas;
+  }
+  
+  comprarEntradas() {
+    this.calcularTotal();
+
+    // Mostrar mensaje de éxito
+    this.compraExitosa = true;
+    this.mensajeExito = `¡Has comprado ${this.cantidadEntradas} entrada(s) para el concierto!`;
+  }
 
 }
--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -7,22 +7,38 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  cliente={user:'',password:''}
-  errorMessage:string='';
-  constructor(private router:Router) { }
+  cliente = { user: '', password: '' };
+  errorMessage: string = '';
+  Message: string = '';
+
+  constructor(private router: Router) {}
 
   ngOnInit() {
-  
+    console.log((this.Message = 'Bienvenido a la pagina de login'));
   }
-  onSubmit():Boolean {
-    if(this.cliente.user === 'admin'&& this.cliente.password === 'admin'){
-      this.router.navigate(['/home'])
-      return true;
-    }
-    else{
-      this.errorMessage="usuario o contraseña incorrecta"
-    }
-    return false;
 
+  onSubmit(): boolean {
+    // Verifica los valores en la consola
+    console.log('Usuario ingresado: ', this.cliente.user);
+    console.log('Contraseña ingresada: ', this.cliente.password);
+    
+    // Verificar si los campos están vacíos
+    if (!this.cliente.user || !this.cliente.password) {
+      this.errorMessage = 'Todos los campos son obligatorios';
+      return false;
+    }
+    // Validación de usuario y contraseña
+    if (this.cliente.user === 'admin' && this.cliente.password === 'admin') {
+      // Almacenar el usuario en el localStorage
+      localStorage.setItem('usuario', this.cliente.user);
+      // Almacenar un token en el localStorage
+      localStorage.setItem('token', 'some-auth-token');
+      
+      this.router.navigate(['/home']);
+      return true;
+    } else {
+      this.errorMessage = 'Usuario o contraseña incorrecta';
+      return false;
+    }
   }
 }
